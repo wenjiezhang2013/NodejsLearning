2380c4f47c6a54177d23a826b3bb9759f8c8ca15
wenjiezhang2013 <wenjiezhang2013@gmail.com>
2014-08-31 11:39:58 -0700

    Simple routing...

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var http = require("http");
-
+var fs = require("fs");
 
 console.log("Starting....");
 var server = http.createServer(function(req, resp){
     console.log("Received request: " + req.url);
-    resp.writeHead(200, {"Content-type": "text/plain"});
-    resp.end("Hello World!")
+    fs.readFile("./route/" + req.url, function(err, data){
+        if(err){
+            resp.writeHead(400, {"Content-type": "text/plain"});
+            resp.end("Sorry, the page was not found!");
+        }
+        else{
+            resp.writeHead(200, {"Content-type": "text/html"});
+            resp.end(data);
+        }
+    })
 });
 
 
